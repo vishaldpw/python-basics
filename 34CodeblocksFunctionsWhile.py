e2d76d60945f71adc626f5eaaef89d067d7f5de2
Vishal Singh Chauhan <70084496+vishaldpw@users.noreply.github.com>
2023-09-16 17:12:12 +0530

    edit 34

--- 34CodeblocksFunctionsWhile.py
+++ 34CodeblocksFunctionsWhile.py
@@ -1,4 +1,10 @@
+
+### Function ##########
 def my_function():
     name = input("whats your name\n")
     print(f"hi {name} whats up ")
 my_function()
+
+### While Loop
+# while something_is_true:
+#     do this
