174058cec9cfcbd39ce5b570a505599bbcd2e6a5
Vishal Singh Chauhan <70084496+vishaldpw@users.noreply.github.com>
2023-12-21 13:02:06 +0530

    Update 08BMI-calculator.py

--- 08BMI-calculator.py
+++ 08BMI-calculator.py
@@ -1,5 +1,6 @@
 weight = input("enter your weight in KGs: \n")
 Height = input("enter your height in meters: \n")
+## ignore this line###
 print(type(weight))
 print(type(Height))
 w = float(weight)
